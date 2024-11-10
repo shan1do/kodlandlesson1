113b1606214934535fd77e2b4108da9d84951331
shan1do <kravilnik@gmail.com>
2024-11-10 17:36:37 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,90 @@
-import random
-simbol = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-score = int(input("Количество символов в пароле "))
-password = ""
 
-for i in range(score):
-    password += random.choice(simbol)
+import telebot
+import random
+from main import generate_password
+text_messages = {
+    'welcome':
+        u'Please welcome {name}!\n\n'
+        u'This chat is intended for questions about and discussion of the pyTelegramBotAPI.\n'
+        u'To enable group members to answer your questions fast and accurately, please make sure to study the '
+        u'project\'s documentation (https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md) and the '
+        u'examples (https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples) first.\n\n'
+        u'I hope you enjoy your stay here!',
+
+    'info':
+        u'My name is TeleBot,\n'
+        u'I am a bot that assists these wonderful bot-creating people of this bot library group chat.\n'
+        u'Also, I am still under development. Please improve my functionality by making a pull request! '
+        u'Suggestions are also welcome, just drop them in this group chat!',
+
+    'wrong_chat':
+        u'Hi there!\nThanks for trying me out. However, this bot can only be used in the pyTelegramAPI group chat.\n'
+        u'Join us!\n\n'
+        u'https://telegram.me/joinchat/067e22c60035523fda8f6025ee87e30b'
+}
+def fiftyfifty():
+    return random.choice(["Орёл", "Решка"])
+
+def smile():
+    return random.choice([":0", ";)", "oo", ":>"])
+token = ""
+
+    # Замени 'TOKEN' на токен твоего бота
+    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot(token)
     
-print(password)
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, message)
+    print(message)
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+    
+@bot.message_handler(commands = ['generate_password'])
+def gen_pas(message):
+    bot.reply_to(message, generate_password(8))
+    
+@bot.message_handler(commands = ['smile'])
+def gen_pas(message):
+    bot.reply_to(message, smile())  
+    
+@bot.message_handler(commands = ['monet'])
+def gen_pas(message):
+    bot.reply_to(message, fiftyfifty())
+    
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)    
+    
+@bot.message_handler(commands=['go'])
+def on_start(message):
+    bot.reply_to(message, text_messages['wrong_chat'])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+           
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+   
+
+    
+    
+
+print("Бот запущен")
+bot.polling()
 
